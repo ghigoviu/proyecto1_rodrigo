bd34d2f5cdb3ade58fd555457fb7da93dd2e91c8
ghigoviu <ghigoviu@gmail.com>
2024-05-11 18:46:56 -0600

    Iniciar proyecto

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <?php
+        echo "<h1>Titulo del blog</h1>";
+        echo "<p>En algún lugar de Tejalpa, cuyo nombre no quiero recordar</p>";
+        $i = 0;
+        for ($i=1; $i < 11; $i++){
+            $i++;
+            echo $i." ";
+        }
+
+    ?>
+</html>
